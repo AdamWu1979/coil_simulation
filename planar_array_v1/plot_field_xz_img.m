2d1bc6f7839e903f96c9206baebba59a779ef374
alexfoias <alexandrufoias@gmail.com>
2015-08-05 09:57:07 -0400

    opt_3

--- planar_array_v1/plot_field_xz_img.m
+++ planar_array_v1/plot_field_xz_img.m
@@ -1,11 +1,7 @@
-function plot_field_xz_img(B1,tpy,xp,px,zp,pz)
+function plot_field_xz_img(B1,tpy,xp,zp)
 ihm(:,:)=B1(:,tpy,:);
 ihm=ihm';
 imagesc(ihm);
-set(gca,'XTick',0:10:size(xp,2))
-set(gca,'XTickLabel',min(xp)*100:10*px*100:max(xp)*100)
-set(gca,'YTick',0:10:size(zp,2))
-set(gca,'YTickLabel',min(zp)*100:10*pz*100:max(zp)*100)
 title('B1 sensitivity profile')
 xlabel('X-axis [cm]')
 ylabel('Z-axis [cm]')
